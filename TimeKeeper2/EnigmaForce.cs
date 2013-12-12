487137702d6f48abf6b4a4196a697448a7811ebb
bossinc <ajhack93@gmail.com>
2013-12-11 21:45:01 -0600

    Initial Commit

---
+++ TimeKeeper2/EnigmaForce.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Data;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace TimeKeeper2
+{
+    class EnigmaForce
+    {
+        // Change these keys
+        private byte[] Key = { 191, 234, 234, 205, 27, 59, 153, 107, 120, 242, 97, 84, 253, 43, 43, 230, 77, 150, 143, 232, 105, 183, 227, 97, 149, 171, 54, 29, 89, 163, 111, 246 };
+        private byte[] Vector = { 77, 192, 79, 130, 188, 112, 149, 50, 13, 131, 217, 79, 141, 235, 235, 60 };
+
+
+        private ICryptoTransform EncryptorTransform, DecryptorTransform;
+        private System.Text.UTF8Encoding UTFEncoder;
+
+        public EnigmaForce()
+        {
+            //This is our encryption method
+            RijndaelManaged rm = new RijndaelManaged();
+
+            //Create an encryptor and a decryptor using our encryption method, key, and vector.
+            EncryptorTransform = rm.CreateEncryptor(this.Key, this.Vector);
+            DecryptorTransform = rm.CreateDecryptor(this.Key, this.Vector);
+
+            //Used to translate bytes to text and vice versa
+            UTFEncoder = new System.Text.UTF8Encoding();
+        }
+
+        /// Encrypt some text and return an encrypted byte array.
+        public byte[] Encrypt(string TextValue)
+        {
+            //Translates our text value into a byte array.
+            Byte[] bytes = UTFEncoder.GetBytes(TextValue);
+
+            //Used to stream the data in and out of the CryptoStream.
+            MemoryStream memoryStream = new MemoryStream();
+
+            /*
+             * We will have to write the unencrypted bytes to the stream,
+             * then read the encrypted result back from the stream.
+             */
+            #region Write the decrypted value to the encryption stream
+            CryptoStream cs = new CryptoStream(memoryStream, EncryptorTransform, CryptoStreamMode.Write);
+            cs.Write(bytes, 0, bytes.Length);
+            cs.FlushFinalBlock();
+            #endregion
+
+            #region Read encrypted value back out of the stream
+            memoryStream.Position = 0;
+            byte[] encrypted = new byte[memoryStream.Length];
+            memoryStream.Read(encrypted, 0, encrypted.Length);
+            #endregion
+
+            //Clean up.
+            cs.Close();
+            memoryStream.Close();
+
+            return encrypted;
+        }
+        /// Decryption when working with byte arrays.    
+        public string Decrypt(byte[] EncryptedValue)
+        {
+            #region Write the encrypted value to the decryption stream
+            MemoryStream encryptedStream = new MemoryStream();
+            CryptoStream decryptStream = new CryptoStream(encryptedStream, DecryptorTransform, CryptoStreamMode.Write);
+            decryptStream.Write(EncryptedValue, 0, EncryptedValue.Length);
+            decryptStream.FlushFinalBlock();
+            #endregion
+
+            #region Read the decrypted value from the stream.
+            encryptedStream.Position = 0;
+            Byte[] decryptedBytes = new Byte[encryptedStream.Length];
+            encryptedStream.Read(decryptedBytes, 0, decryptedBytes.Length);
+            encryptedStream.Close();
+            #endregion
+            return UTFEncoder.GetString(decryptedBytes);
+        }
+    }
+}
